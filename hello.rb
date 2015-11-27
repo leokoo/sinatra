131bd4e4937557c3dfd2caf841467448779dbaa2
Leo Koo <leokoo@gmail.com>
2015-11-27 17:36:49 +0800

    exercises done

--- hello.rb
+++ hello.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/hello' do
   "Hello Sinatra!"
+end
+
+get '/:name' do
+  name = params[:name]
+  "Hello #{name}"
+end
+
+get '/:one/:two/:three' do
+	"first: #{params[:one]}, second: #{params[:two]}, three: #{params[:three]}"
+end
+
+get '/what/time/is/it/in/:number/hours' do
+  number = params[:number].to_i
+  time = Time.now + number * 3600
+  "The time in #{number} hours will be #{time.strftime('%I:%M %p')}"
 end
